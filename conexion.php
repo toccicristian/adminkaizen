2d1ff21fd0778ac2ea031371d36eaccf9e73ca5f
Cristian Tocci <cristian@pcofi.pcofi.org>
2024-11-18 17:31:50 -0300

    quitar datos conexion

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-    $conexion = mysqli_connect("localhost","adminkaizen","b7tllee8", "adminkaizen") or die ('No se pudo conectar a la base de datos, por favor comuníquese con el administrador del sistema');
+    $conexion = mysqli_connect("localhost","root","", "adminkaizen") or die ('No se pudo conectar a la base de datos, por favor comuníquese con el administrador del sistema');
 ?>
