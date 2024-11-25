504ec638c6b42838b23b1db78c42fa4fcfb6d6b9
Cristian Tocci <cristian@pcofi.pcofi.org>
2024-11-25 17:19:53 -0300

    ui en tareas 2

--- ajax.php
+++ ajax.php
@@ -18,88 +18,86 @@
 
         $idrol=$_SESSION['idrol'];
         $username=$_SESSION['nombre'];
-      
+    } 
 ?>
+
+
+
 <section>
     <article class="tabla-resultados tabla-no-centrada">
-        <!-- <table class="tabla-resultados">
-            <tr>
+
+        <table class="table table-stripped">
+            <thead class="thead-light">
+                <tr>
                 <th class="campo-resultados">Nombre</th>
                 <th class="campo-resultados">Inicio</th>
                 <th class="campo-resultados">Mejor Fin</th>
                 <th class="campo-resultados">Peor Fin</th>
                 <th class="campo-resultados"></th>
                 <th class="campo-resultados"></th>
-            </tr> -->
+                </tr>
+            </thead>
 
-            <table class="table table-stripped">
-                <thead class="thead-light">
-                    <tr>
-                    <th class="campo-resultados">Nombre</th>
-                    <th class="campo-resultados">Inicio</th>
-                    <th class="campo-resultados">Mejor Fin</th>
-                    <th class="campo-resultados">Peor Fin</th>
-                    <th class="campo-resultados"></th>
-                    <th class="campo-resultados"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                </tbody>
 
-            
-            
-<?php
-    while ($resultado = MySQLi_fetch_array($consulta)) {
-        if(($resultado['eliminada']==0) || ($_POST['mostrareliminadas']!='false')){
-            
-        
-?>
-            <tr onclick='fill("<?php echo $resultado['Nombre']; ?>")'>
-                <td class="campo-resultados"><?php echo $resultado['Nombre'];   ?></td>
-                <td class="campo-resultados"><?php echo $resultado['Inicio'];   ?></td>
-                <td class="campo-resultados"><?php echo $resultado['MejorFin']; ?></td>
-                <td class="campo-resultados"><?php echo $resultado['PeorFin'];  ?></td>
-<?php
-        $ownerid=$resultado['OwnerId'];
-        $consultaEsOwner=mysqli_query($conexion, "SELECT IdUsuario FROM USUARIO WHERE USUARIO.Nombre = '$username' AND USUARIO.IdUsuario = '$ownerid'");
-        $consultaOutRol=mysqli_query($conexion, "SELECT IdUsuario FROM USUARIO WHERE USUARIO.IdUsuario = '$ownerid' AND USUARIO.ROL_IdRol > '$idrol'");
-        if((mysqli_num_rows($consultaEsOwner)!=0) || (mysqli_num_rows($consultaOutRol)!=0)){
-            ?>  
-<?php 
-            if($resultado['eliminada']==1){
-?>
-                <td class="campo-resultados">
-                    <form action="restaurartarea.php" method="post">
-                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
-                        <button type="submit" class="btn btn-secondary">Restaurar</button>
-                    </form>
-                </td>
-<?php
-            }
-            else{
-?>
-                <td class="campo-resultados">
-                    <form action="eliminartarea.php" method="post" >
-                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
-                        <button type="submit" class="btn btn-danger">Eliminar</button>
-                    </form>
-                </td>
-                <td class="campo-resultados">
-                    <form action="asignaciontarea.php" method="post" target="_blank">
-                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
-                        <button type="submit" class="btn btn-primary" >Asignar</button>
-                    </form>
-                </td>                
-<?php
-            }
-        }
+            <tbody>
+                <?php while ($resultado = MySQLi_fetch_array($consulta)) {
+                        if(($resultado['eliminada']==0) || ($_POST['mostrareliminadas']!='false')){  
+                            
+                            ?>
+                            <tr onclick='fill("<?php echo $resultado['Nombre']; ?>")'>
+                                <td><?php echo $resultado['Nombre'];   ?></td>
+                                <td><?php echo $resultado['Inicio'];   ?></td>
+                                <td><?php echo $resultado['MejorFin']; ?></td>
+                                <td><?php echo $resultado['PeorFin'];  ?></td>
 
-?>
-            </tr>
-<?php
-    }}}
-?>
+                                <?php
+                                $ownerid=$resultado['OwnerId'];
+                                $consultaEsOwner=mysqli_query($conexion, "SELECT IdUsuario FROM USUARIO 
+                                                                            WHERE USUARIO.Nombre = '$username' 
+                                                                            AND USUARIO.IdUsuario = '$ownerid'");
+                                $consultaOutRol=mysqli_query($conexion, "SELECT IdUsuario FROM USUARIO 
+                                                                            WHERE USUARIO.IdUsuario = '$ownerid' 
+                                                                            AND USUARIO.ROL_IdRol > '$idrol'");
+                                if((mysqli_num_rows($consultaEsOwner)!=0) || (mysqli_num_rows($consultaOutRol)!=0)){
+                                    if($resultado['eliminada']==1){
+                                ?>
+
+                                <td>
+                                    <form action="restaurartarea.php" method="post">
+                                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
+                                        <button type="submit" class="btn btn-secondary">Restaurar</button>
+                                    </form>
+                                </td>
+                                <?php
+                                    }else{
+                                ?>
+                                <td>
+                                    <form action="eliminartarea.php" method="post" >
+                                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
+                                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="asignaciontarea.php" method="post" target="_blank">
+                                        <input type="hidden" name="taskid" value=<?php echo $resultado['idTarea'] ?>>
+                                        <button type="submit" class="btn btn-primary" >Asignar</button>
+                                    </form>
+                                </td>                
+                                <?php
+                                    }
+                                }
+                                else{
+                                    ?>
+                                    <td></td>
+                                    <td></td>
+                                    <?php
+                                }
+                                ?>
+                            </tr>
+                        <?php
+                        }
+                    }?>
+            </tbody>
         </table>
     </article>
 </section>
