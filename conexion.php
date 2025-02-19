8ddfd93d0c1a00b82bcd61bf21cb8a6871633e18
Cristian Tocci <cristian@cristian-g480.cristian.net>
2025-02-19 16:12:19 -0300

    conexion.php para lampp

--- conexion.php
+++ conexion.php
@@ -1,10 +1,8 @@
 <?php
-
 	$dbhost = 'localhost';
-	$dbuser = 'c2770751_db1';
-	$dbpass = 'susuwoKE73';
-	$dbname = 'c2770751_db1';
+	$dbuser = 'root';
+	$dbpass = '';
+	$dbname = 'adminkaizen';
 
 	$conexion = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Ocurrio un error al conectarse al servidor mysql');
-	//mysql_select_db($dbname);
 ?>
